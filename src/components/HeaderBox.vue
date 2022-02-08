bfb94022807bbfb4e421ef5ff0ac20fd93e496d5
ThubeesanKugathas <Thubeesan@outlook.com>
2022-02-08 11:46:15 +0100

    destructuring HeaderBox with comments

--- src/components/HeaderBox.vue
+++ src/components/HeaderBox.vue
@@ -1,6 +1,14 @@
 <template>
     <header>
-
+        <!-- IMG BACKGROUND SVG's -->
+        <!-- <img src="../assets/header-img/jumbotron-background-wave-small.svg" alt="small wave">
+        <img src="../assets/header-img/jumbotron-background-wave-medium.svg" alt="medium wave">
+        <img src="../assets/header-img/jumbotron-background-wave-big.svg" alt="big wave"> -->
+        <div class="container">
+            <!-- navbar component -->
+            <!-- central title + button -->
+            <!-- 3 img  -->
+        </div>
     </header>
 </template>
 
@@ -11,5 +19,16 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '../style/variables.scss';
 
+    header {
+        background-color: #1f2154;
+        height: 500px;
+
+        .container {
+            width: $c-width;
+            margin: 0 auto;
+        }
+        
+    }
 </style>
