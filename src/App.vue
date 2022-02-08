ab7640789aa2f886e7a38c47fc72c7e51f1b492f
ThubeesanKugathas <Thubeesan@outlook.com>
2022-02-08 13:26:51 +0100

    footerBox main structure

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,11 @@
   <div id="app">
     <header-box :navList="this.navbarList" :logoString="this.navbarLogo"/>
     <main-box />
-    <footer-box />
+    <footer-box :street="this.addressData.street" 
+      :number="this.addressData.number" 
+      :email="this.addressData.email"
+      :exploreLinks="this.explore"
+      :informationLinks="this.information"/>
   </div>
 </template>
 
@@ -29,7 +33,27 @@ export default {
         'Blog',
         'Shop',
       ],
-      navbarLogo: 'light-logo'
+      navbarLogo: 'light-logo',
+      // footer data
+      addressData: {
+        street: '382 NE 191st St # 87394 Miami, FL-33179-3899',
+        number: '+1(305)547-9909(9am-5pm EST, Monday-Friday)',
+        email: 'support@maxcoach.com'
+      },
+      explore: [
+        'Start here',
+        'Success story',
+        'Blog',
+        'Courses',
+        'About us',
+        'Contact us'
+      ],
+      information: [
+        'Membership',
+        'Puchase guide',
+        'Privacy policy',
+        'Terms of services'
+      ],
     }
   }
 }
--- src/components/FooterBox.vue
+++ src/components/FooterBox.vue
@@ -1,15 +1,100 @@
 <template>
     <footer>
-        
+        <div class="container">
+            <ul>
+                <li>ADDRESS
+                    <ul>
+                        <li>{{street}}</li>
+                        <li>{{number}}</li>
+                        <li>{{email}}</li>
+                        <li>
+                            <i class="fab fa-facebook-square"></i>
+                            <i class="fab fa-twitter"></i>
+                            <i class="fab fa-instagram"></i>
+                            <i class="fab fa-linkedin"></i>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+            <div class="right-side">
+                <ul>
+                    <li>EXPLORE
+                        <ul class="explore">
+                            <li v-for="(link, index) in exploreLinks" :key="index">{{link}}</li>
+                        </ul>
+                    </li>
+                    <li>INFORMATION
+                        <ul>
+                            <li v-for="(link, index) in informationLinks" :key="index">{{link}}</li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <p>
+            <i class="far fa-copyright"></i>
+            2020 Maxcoach.All Rights Reserved
+        </p>
+        <div class="freccia">freccia</div>
     </footer>
 </template>
 
 <script>
 export default {
-    
+    props: {
+        street: String,
+        number: String,
+        email: String,
+        exploreLinks: Array,
+        informationLinks: Array,
+    }
 }
 </script>
 
 <style lang="scss" scoped>
+    @import '../style/variables.scss';
+    @import url('https://use.fontawesome.com/releases/v5.7.1/css/all.css');
+
+    footer {
+        background-color: $grey-one;
+        margin-top: 200px;
+        position: relative;
+        padding: 50px;
+
+        .container {
+            width: $c-width;
+            margin: 0 auto;
+            display: flex;
+            justify-content: space-between;
+
+            .right-side {
+                
+                >ul {
+                    display: flex;
+
+                    .explore {
+                        display: flex;
+                        flex-wrap: wrap;
+
+                        li {
+                            width: 50%;
+                        }
+                    }
+                }
+            }
+
+        }
+
+        p {
+            text-align: center;
+        }
+
+        .freccia {
+            position: absolute;
+            right: 20px;
+            bottom: 20px;
+            transform: translate(-50% -50%);
+        }
+    }
 
 </style>
