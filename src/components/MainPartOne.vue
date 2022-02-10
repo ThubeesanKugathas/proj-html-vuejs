f2c632d6d07783156032624fb752d828fe5ab949
ThubeesanKugathas <Thubeesan@outlook.com>
2022-02-10 12:37:41 +0100

    Utility classes added in MainPartOne

--- src/components/MainPartOne.vue
+++ src/components/MainPartOne.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div class="container">
+        <div class="u_container u_text-centered u_position-relative container">
             <div class="text">
                 <h3>
                     Let passion and determination be the guide<br/>
@@ -11,22 +11,22 @@
                 <p>/Founder & CEO</p>
             </div>
 
-            <div class="text-blocks-container">
+            <div class="u_d-flex u_justify-between text-blocks-container">
                 <!-- single stat in v-for -->
-                <div v-for="(stat, i) in stats" :key="i" class="text-block">
+                <div class="text-block" v-for="(stat, i) in stats" :key="i">
                     <h2>{{stat.number}}</h2>
                     <p>{{stat.data}}</p>
                 </div>
             </div>
 
-            <div class="ball-container">
-                <div class="ball ball-one"></div>
-                <div class="ball ball-two"></div>
-                <div class="ball ball-three"></div>
-                <div class="ball ball-four"></div>
-                <div class="ball ball-five"></div>
-                <div class="ball ball-six"></div>
-                <div class="ball ball-seven"></div>
+            <div class="u_container u_position-relative u_position-absolute ball-container">
+                <div class="u_radius-50 u_position-absolute ball-one"></div>
+                <div class="u_radius-50 u_position-absolute ball-two"></div>
+                <div class="u_radius-50 u_position-absolute ball-three"></div>
+                <div class="u_radius-50 u_position-absolute ball-four"></div>
+                <div class="u_radius-50 u_position-absolute ball-five"></div>
+                <div class="u_radius-50 u_position-absolute ball-six"></div>
+                <div class="u_radius-50 u_position-absolute ball-seven"></div>
             </div>
         </div>
     </div>
